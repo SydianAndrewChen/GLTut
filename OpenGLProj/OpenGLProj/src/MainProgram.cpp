80c57c5c1abee320341ef81584817f895a384142
SydianAndrewChen <65159293+SydianAndrewChen@users.noreply.github.com>
2021-01-26 20:14:59 +0800

    Clean code

--- OpenGLProj/OpenGLProj/src/MainProgram.cpp
+++ OpenGLProj/OpenGLProj/src/MainProgram.cpp
@@ -6,12 +6,12 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "Shader.h"
-#include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 #include "Texture.h"
+#include "Renderer.h"
 
 #include "Camera.h"
 
@@ -22,8 +22,9 @@
 #include "../vendor/imgui/imgui_impl_opengl3.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void processInput(GLFWwindow* window, Camera & camera);
 void mouse_call_back(GLFWwindow* window, double xpos, double ypos);
+void scroll_back(GLFWwindow* window, double xoffset, double yoffset);
 
 float vertices[] = {
 	// Œª÷√				  Œ∆¿Ì
@@ -83,71 +84,15 @@ glm::vec3 cubePositions[] = {
 	glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
-unsigned int indices[] = {
-	0,1,3,
-	1,2,3
-};
 
 int screenWidth = 800;
 int screenHeight = 600;
+float lastX = screenWidth / 2;
+float lastY = screenHeight / 2;
 
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-
 Camera camera(cameraPos, cameraFront);
-
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-float cameraSpeed = 2.5f * deltaTime;
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-bool cameraEnable = true;
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.moveForward();
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.moveLeft();
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.moveBack();
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.moveRight();
-	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
-		cameraFront = glm::normalize(-cameraPos);
-
-	camera.setEnable(glfwGetKey(window, GLFW_KEY_E) != GLFW_PRESS);
-}
-
-float fov = 30.0f;
-void scroll_back(GLFWwindow* window, double xoffset, double yoffset) 
-{
-	camera.zoom(yoffset);
-}
-
-float lastX = screenWidth / 2, lastY = screenHeight / 2;
-float yaw = 0, pitch = 0;
-
-bool firstMouse = true;
-void mouse_call_back(GLFWwindow* window, double xpos, double ypos)
-{
-	//if (firstMouse){
-	//	lastX = xpos;
-	//	lastY = ypos;
-	//	firstMouse = false;
-	//}
-		float xoffset = xpos - lastX;
-		float yoffset = lastY - ypos;
-		lastX = xpos;
-		lastY = ypos;
-
-		camera.rotate(xoffset, yoffset);
-}
 
 int main() {
 	glfwInit();
@@ -172,7 +117,7 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetCursorPosCallback(window, mouse_call_back);
 
 	glfwSetScrollCallback(window, scroll_back);
@@ -182,7 +127,6 @@ int main() {
 		Texture surTex("./textures/awesomeface.png");
 
 		VertexArray va;
-		IndexBuffer eb(indices, 6);
 
 		VertexBuffer vb(vertices, sizeof(vertices));
 		VertexBufferLayout vbl;
@@ -192,20 +136,10 @@ int main() {
 		va.unbind();
 		vb.unbind();
 
-		Shader ourShader("./src/shader.vs", "./src/shader.fs");
+		Shader ourShader("./src/shaders/shader.vs", "./src/shaders/shader.fs");
 		ourShader.use();
 		ourShader.setInt("boxTex", 0);
 		ourShader.setInt("surTex", 1);
-
-		glm::mat4 model;
-		ourShader.setFloat4_4("model",model);
-
-		glm::mat4 view;
-		ourShader.setFloat4_4("view", view);
-
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(camera.getFov()), (float) screenWidth / screenHeight, 0.1f, 100.0f);
-		ourShader.setFloat4_4("perspProj", projection);
 
 		Renderer renderer;
 		glEnable(GL_DEPTH_TEST);
@@ -219,15 +153,17 @@ int main() {
 		bool show_another_window = false;
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+		float deltaTime = 0.0f;
+		float lastFrame = 0.0f;
 		while (!glfwWindowShouldClose(window))
 		{
 			float currentFrame = glfwGetTime();
 			deltaTime = currentFrame - lastFrame;
 			lastFrame = currentFrame;
-			cameraSpeed = 2.5f * deltaTime;
+			float cameraSpeed = 2.5f * deltaTime;
 			camera.setSpeed(cameraSpeed);
 
-			processInput(window);
+			processInput(window, camera);
 
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			renderer.clear();
@@ -241,10 +177,10 @@ int main() {
 			surTex.bind(1);
 
 			va.bind();
-			view = camera.getLookAt();
+			glm::mat4 view = camera.getLookAt();
 			ourShader.setFloat4_4("view", view);
 
-			projection = glm::perspective(glm::radians(fov), (float)screenWidth / screenHeight, 0.1f, 100.0f);
+			glm::mat4 projection = glm::perspective(glm::radians(camera.getFov()), (float)screenWidth / screenHeight, 0.1f, 100.0f);
 			ourShader.setFloat4_4("perspProj", projection);
 
 			for (unsigned int i = 0; i < 10; ++i) 
@@ -281,4 +217,37 @@ int main() {
 	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); }
+
+void processInput(GLFWwindow* window, Camera & camera) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.moveForward();
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.moveLeft();
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.moveBack();
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.moveRight();
+	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) { }
+
+	camera.setEnable(glfwGetKey(window, GLFW_KEY_E) != GLFW_PRESS);
+}
+
+void scroll_back(GLFWwindow* window, double xoffset, double yoffset) { camera.zoom(yoffset); }
+
+void mouse_call_back(GLFWwindow* window, double xpos, double ypos)
+{
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.rotate(xoffset, yoffset);
 }
